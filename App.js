4827b833024b914cf706bf0c7d5d65be90acf77d
Stephan DuVal <stephan.duval@gmail.com>
2022-03-08 14:14:01 -0800

    First commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  StyleSheet,
+  Text,
+   View,
+} from 'react-native';
+
+
+
+
+const App =  () => {
+  
+
+  
+
+  return (
+   <View>
+<Text>Programming With Mash Tutorial</Text>
+   </View>
+  );
+};
+
+const styles = StyleSheet.create({
+body: {
+  backgroundColor: '#ffffff',
+},
+});
+
+export default App;
